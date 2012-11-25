3691765ba261dc172382ef3394bccb8e19aa1a1f
Jocelyn FRIDAY <jf374@mac1-031-m.cs.st-andrews.ac.uk>
2012-11-25 15:56:48 +0000

    Initial Commit

---
+++ Solar_System_II/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Solar_System_II
+//
+//  Created by Jocelyn FRIDAY on 25/11/2012.
+//  Copyright (c) 2012 Jocelyn FRIDAY. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
